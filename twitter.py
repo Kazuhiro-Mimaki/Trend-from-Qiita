6223551a9e58a6ee0f53a782691d453847491e3a
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2020-11-28 08:45:19 +0900

    clockの設定

--- twitter.py
+++ twitter.py
@@ -5,50 +5,51 @@ import tweepy
 import settings
 import random
 
-CK_KEY = settings.CK
-CS_KEY = settings.CS
-AT_KEY = settings.AT
-AS_KEY = settings.AS
-SLACK_WEB_HOOK = settings.SWH
+def post():
+  CK_KEY = settings.CK
+  CS_KEY = settings.CS
+  AT_KEY = settings.AT
+  AS_KEY = settings.AS
+  SLACK_WEB_HOOK = settings.SWH
 
-# Twitterオブジェクトの生成
-auth = tweepy.OAuthHandler(CK_KEY, CS_KEY)
-auth.set_access_token(AT_KEY, AS_KEY)
-api = tweepy.API(auth)
+  # Twitterオブジェクトの生成
+  auth = tweepy.OAuthHandler(CK_KEY, CS_KEY)
+  auth.set_access_token(AT_KEY, AS_KEY)
+  api = tweepy.API(auth)
 
-item_json = []
-result = []
+  item_json = []
+  result = []
 
-# 取得するページ番号
-page_number = random.randrange(1, 101)
+  # 取得するページ番号
+  page_number = random.randrange(1, 101)
 
-qiitaUrl = f"https://qiita.com/search?page={page_number}&q=%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0&sort=like"
+  qiitaUrl = f"https://qiita.com/search?page={page_number}&q=%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0&sort=like"
 
-res = requests.get(qiitaUrl)
-soup = BeautifulSoup(res.text, "html.parser")
+  res = requests.get(qiitaUrl)
+  soup = BeautifulSoup(res.text, "html.parser")
 
-items = soup.find_all(class_="searchResult_main")
+  items = soup.find_all(class_="searchResult_main")
 
-# 取得する記事の番号
-item_number = random.randrange(1, 11)
+  # 取得する記事の番号
+  item_number = random.randrange(1, 11)
 
-title = items[item_number].find(class_="searchResult_itemTitle")
-title_url = title.find("a")
-url = title_url['href']
-text = title.get_text() + '\n'
-text += 'https://qiita.com' + url + '\n'
-text += '\n'
+  title = items[item_number].find(class_="searchResult_itemTitle")
+  title_url = title.find("a")
+  url = title_url['href']
+  text = title.get_text() + '\n'
+  text += 'https://qiita.com' + url + '\n'
+  text += '\n'
 
-# twitterにpost
-try:
-  api.update_status(text)
-except Exception as e:
-  print(e)
+  # twitterにpost
+  try:
+    api.update_status(text)
+  except Exception as e:
+    print(e)
 
-# slackにpost
-requests.post(SLACK_WEB_HOOK, data = json.dumps({
-    'text': text,
-    'unfurl_links': u'true',
-    'username': u'Buzzrita',
-    'icon_emoji': u':buzzrita:',
-}))
+  # slackにpost
+  requests.post(SLACK_WEB_HOOK, data = json.dumps({
+      'text': text,
+      'unfurl_links': u'true',
+      'username': u'Buzzrita',
+      'icon_emoji': u':buzzrita:',
+  }))
