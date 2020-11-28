c69704d42ed1c3a55d3b253d5191afca0425bfb1
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2020-11-28 12:04:16 +0900

    slackに投稿されるかテスト

--- twitter.py
+++ twitter.py
@@ -40,10 +40,10 @@ text += 'https://qiita.com' + url + '\n'
 text += '\n'
 
 # twitterにpost
-try:
-  api.update_status(text)
-except Exception as e:
-  print(e)
+# try:
+#   api.update_status(text)
+# except Exception as e:
+#   print(e)
 
 # slackにpost
 requests.post(SLACK_WEB_HOOK, data = json.dumps({
